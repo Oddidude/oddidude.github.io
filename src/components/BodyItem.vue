de778a790a213d9fbf04a37c8d6bee6c7b4d7f0c
Oddidude <luke.cheungster@gmail.com>
2025-03-19 21:53:35 +0000

    Adding entry animations for cards

--- src/components/BodyItem.vue
+++ src/components/BodyItem.vue
@@ -2,14 +2,37 @@
   import CardItem from './CardItem.vue';
 
   import * as contentArr from '../content'
+  import gsap from 'gsap';
+
+  const onEnter = (el: Element, done: () => void) => {
+    gsap.from(el, {
+      ease: 'back.out(0.6)',
+      transform: 'translateY(1000%)',
+      delay: parseInt((el as HTMLElement).dataset.index!) * 0.1,
+      onComplete: done
+    })
+  }
+
+  const redirect = (link: string) => () => window.location.href = link
 </script>
 
 <template>
-  <div class="body">
-    <div v-for="content in contentArr" :key="content.title">
-      <CardItem v-bind="content" />
-    </div>
-  </div>
+    <TransitionGroup
+      appear
+      tag="ul"
+      :css="false"
+      class="body"
+      @enter="onEnter"
+    >
+      <li
+        v-for="(content, index) in Object.values(contentArr)"
+        :key="content.title"
+        :data-index="index"
+        @click="redirect(content.link)"
+      >
+        <CardItem v-bind="content"/>
+      </li>
+    </TransitionGroup>
 </template>
 
 <style scoped>
@@ -21,5 +44,6 @@
   gap: 2%;
   margin-top: 1%;
   padding: 0 1%;
+  list-style-type: none;
 }
 </style>
--- src/components/CardItem.vue
+++ src/components/CardItem.vue
@@ -1,15 +1,13 @@
 <script setup lang="ts">
-const { link } = defineProps<{
+defineProps<{
   title: string;
   description: string;
   link: string;
 }>()
-
-const redirect = () => window.location.href = link
 </script>
 
 <template>
-  <div class="card" @click="redirect">
+  <div class="card">
     <div class="card-header">
       <h2>{{ title }}</h2>
     </div>
@@ -24,21 +22,22 @@ const redirect = () => window.location.href = link
   background-color: grey;
   border-radius: 6px;
   padding: 4px 8px;
-  width: 15rem;
+  width: 17rem;
   overflow-x: hidden;
   height: 100%;
-  min-height: 15rem;
+  min-height: 16rem;
   display: flex;
   flex-direction: column;
   justify-content: start;
   align-items: center;
+  transition: transform 0.2s;
 }
 
 .card:hover{
   cursor: pointer;
-  transform: scale(1.02);
+  transform: scale(1.05);
   box-shadow: 2px 2px 5px var(--shadow), -2px -2px 5px var(--shadow);
-  transition: all 0.2s;
+  transition: transform 0.4s;
 }
 
 .card-header {
